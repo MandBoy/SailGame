80a4f8a914981516f68c42c244cf10921c997145
Andreas Lind <32731570+MandBoy@users.noreply.github.com>
2019-04-17 12:08:40 +0200

    Add files via upload

---
+++ Ammunition.java
@@ -0,0 +1,4 @@
+public class Ammunition {
+    String [] types = {"Grapple","Ball","Chicken"};
+    int [] damage = {2};
+}
---
+++ Ship.java
@@ -0,0 +1,11 @@
+public class Ship implements shipInterface{
+    private String name;
+
+
+
+    public void move(){}
+    public void shoot(){}
+    public void reload(){}
+    public void turn(){}
+
+}
